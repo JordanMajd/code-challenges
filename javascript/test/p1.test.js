9ef68b41e7a6ec63030b41fd7c387e18d929dc27
Jordan Majd <jordan.majd@galvanize.com>
2016-05-29 22:52:55 -0600

    reorders so that most optimal p1 test code is at top

--- javascript/test/p1.test.js
+++ javascript/test/p1.test.js
@@ -4,17 +4,27 @@ const p1 = require('../solution/p1');
 const testUtil = require('../src/test.util');
 const expect = require('chai').expect;
 
-describe('s0', function(){
+describe('s2Sum', function(){
 
+  it('sums all occurances of number under given max', function(){
+    expect(p1.s2Sum(3, 1000)).to.equal(166833);
+    expect(p1.s2Sum(5, 1000)).to.equal(99500);
+    expect(p1.s2Sum(15, 1000)).to.equal(33165);
+  });
+
+  testUtil.testExecutionTime(p1.s2Sum, [3, 1000]);
+});
+
+describe('s2', function(){
   it('works with given case', function(){
-    expect(p1.s0(10)).to.equal(23);
+    expect(p1.s2(10)).to.equal(23);
   });
 
   it('finds the sum of all the multiples of 3 or 5 below 1000', function(){
-    expect(p1.s0(1000)).to.equal(233168);
+    expect(p1.s2(1000)).to.equal(233168);
   });
 
-  testUtil.testExecutionTime(p1.s0, [1000]);
+  testUtil.testExecutionTime(p1.s2, [1000]);
 });
 
 describe('s1Sum', function(){
@@ -40,25 +50,15 @@ describe('s1', function(){
   testUtil.testExecutionTime(p1.s1, [1000]);
 });
 
-describe('s2Sum', function(){
+describe('s0', function(){
 
-  it('sums all occurances of number under given max', function(){
-    expect(p1.s2Sum(3, 1000)).to.equal(166833);
-    expect(p1.s2Sum(5, 1000)).to.equal(99500);
-    expect(p1.s2Sum(15, 1000)).to.equal(33165);
-  });
-
-  testUtil.testExecutionTime(p1.s2Sum, [3, 1000]);
-});
-
-describe('s2', function(){
   it('works with given case', function(){
-    expect(p1.s2(10)).to.equal(23);
+    expect(p1.s0(10)).to.equal(23);
   });
 
   it('finds the sum of all the multiples of 3 or 5 below 1000', function(){
-    expect(p1.s2(1000)).to.equal(233168);
+    expect(p1.s0(1000)).to.equal(233168);
   });
 
-  testUtil.testExecutionTime(p1.s2, [1000]);
+  testUtil.testExecutionTime(p1.s0, [1000]);
 });
