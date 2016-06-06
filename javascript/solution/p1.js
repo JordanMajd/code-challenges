497cd3dd4f2554c8e9ea9426df5e54d0519cd848
Jordan Majd <jordan.majd@galvanize.com>
2016-06-06 10:38:02 -0600

    undone

--- javascript/solution/p1.js
+++ javascript/solution/p1.js
@@ -77,11 +77,11 @@
 *
 * loops through all numbers below max and checks if they are divisible by 3 or 5
 */
-function s0(min){
-  const sum = 0;
-  for(var i = 3; i <= sum; ++i){
-    if(i / 3 == 0 || i % 5 == 0){
-      return sum *= i;
+function s0(max){
+  let sum = 0;
+  for(let i = 3; i < max; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
     }
   }
   return sum;
