0a41205b89317a2b66d3810cc528a5cd7e5f985e
Jordan Majd <jordan@madtitanstudios.com>
2017-12-11 17:48:35 -0700

    Impl merge sort

--- algorithms/sorts/merge_sort/src/merge_sort.js
+++ algorithms/sorts/merge_sort/src/merge_sort.js
@@ -1,11 +1,44 @@
 'use strict';
 
-function mergesort(arr){
+module.exports = mergesort;
 
-  if(arr.length > 0){
+// Average: O(n log(n))
+// Worst: O(n log(n))
+// Memory: variable
+// Top Down
+function mergesort(a){
 
+  //copy array into helper
+  let b = a.slice();
+  splitmerge(a, b, 0, a.length);
+
+  return a;
+}
+
+function splitmerge(a, b, low, high){
+
+  if(high - low < 2){
+    return;
   }
 
-  return arr;
-  
+  let mid = Math.floor((high + low) / 2);
+
+  splitmerge(b, a, low, mid);
+  splitmerge(b, a, mid, high);
+  merge(b, a, low, mid, high);
+}
+
+function merge(a, b, low, mid, high){
+
+  let left = low;
+  let right = mid;
+
+  for(let cur = low; cur < high; cur++){
+
+    if(left < mid && (right >= high || a[left] <= a[right])){
+      b[cur] = a[left++];
+    } else {
+      b[cur] = a[right++];
+    }
+  }
 }
